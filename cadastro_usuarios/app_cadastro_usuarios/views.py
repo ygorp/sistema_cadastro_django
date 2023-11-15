21ec2a813bbea8b3f8776dfa70c0b98129156cbe
ygorp <ygor-nogueira@hotmail.com>
2023-11-15 10:46:43 -0300

    criado no views a função para pegar os dados do banco e exibir em uma nova página, criei a página usuarios com uma tabela fazendo um laço de repetiçaõ para percorrer os dados e exibir

--- cadastro_usuarios/app_cadastro_usuarios/views.py
+++ cadastro_usuarios/app_cadastro_usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Usuarios
 
 
 def home(request):
@@ -6,4 +7,14 @@ def home(request):
 
 
 def usuarios(request):
-    pass
+    # Salvar dados no banco de dados
+    novo_usuario = Usuarios()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    # Exibir todos os usuarios ja cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuarios.object.all()
+    }
+    # Retornar os dados da página de listagem de usuarios
+    return render(request, 'usuarios/usuarios.html', usuarios)
