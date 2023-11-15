b3e2a658cf915ec562ef054c87bd6696e50f8618
ygorp <ygor-nogueira@hotmail.com>
2023-11-15 10:32:57 -0300

    criando a rota de listagem de usuarios, criando model com a classe usuarios para criar meu banco de dados, feito makemigrations e migrate

--- cadastro_usuarios/app_cadastro_usuarios/views.py
+++ cadastro_usuarios/app_cadastro_usuarios/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'usuarios/home.html')
+
+
+def usuarios(request):
+    pass
--- cadastro_usuarios/cadastro_usuarios/urls.py
+++ cadastro_usuarios/cadastro_usuarios/urls.py
@@ -3,4 +3,5 @@ from app_cadastro_usuarios import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('usuarios/', views.usuarios, name='listagem_usuarios'),
 ]
